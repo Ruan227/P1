7acc7c53d898571703856ef93ec504203df2c210
Ruan227 <ruansilva.pascoal@gmal.com>
2023-09-27 18:32:44 -0300

    Pronto Prova

---
+++ src/routes/Routes.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from '../screens/home/Home';
+const Stack = createStackNavigator(); 
+
+export default function Router() {
+    return (
+
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'> 
+
+                <Stack.Screen name='Home' component={Home} />
+            
+            </Stack.Navigator>
+            
+        </NavigationContainer>
+    )
+}
+
